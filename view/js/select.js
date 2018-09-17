9e06cd3152d29daf494ad1ef2eae55788981eceb
Lucas Zani <lucas.albzani@gmail.com>
2018-09-16 21:26:00 -0300

    Criação do botao calcular e dos radio buttons de selecao de sexo alem da criação do script calcular.js que recebe as informãcoes do formulario e calcula o 'consumo total de energia'. Ainda com problemas para receber qual radio button está selecionado

--- view/js/select.js
+++ view/js/select.js
@@ -2,20 +2,8 @@ $(document).ready(function () {
     $("#intensidade").on('change', function () {
         $('#exercicio').html('');
         var exercicios = [];
-        if ($('#intensidade').val() == 1) {
-            exercicios = ['Caminhada', 'Abdominal'];
-            var novosExercicios = '';
-
-            exercicios.forEach(function(valor){
-                novosExercicios += '<option value="' + valor + '">' + valor + '</option>';
-            });
-
-            $('#exercicio').html(novosExercicios);
-
-            $('#exercicio').removeAttr('disabled');
-        }
-        else if ($('#intensidade').val() == 2) {
-            exercicios = ['Cooper', 'Flexão'];
+        if ($('#intensidade').val() == 2) {
+            exercicios = ['Caminhada', 'Passear com o cachorro', 'Ir ao mercado a pé','Fazer uma aula de alongamento'];
             var novosExercicios = '';
 
             exercicios.forEach(function(valor){
@@ -27,7 +15,19 @@ $(document).ready(function () {
             $('#exercicio').removeAttr('disabled');
         }
         else if ($('#intensidade').val() == 3) {
-            exercicios = ['Corrida', 'Supino'];
+            exercicios = ['Praticar um esporte no lazer', 'Pedalar sem ficar ofegante', 'Correr sem ficar ofegante', 'Treinar musculação em circuito'];
+            var novosExercicios = '';
+
+            exercicios.forEach(function(valor){
+                novosExercicios += '<option value="' + valor + '">' + valor + '</option>';
+            });
+
+            $('#exercicio').html(novosExercicios);
+
+            $('#exercicio').removeAttr('disabled');
+        }
+        else if ($('#intensidade').val() == 4) {
+            exercicios = ['HIIT', 'Correr até ficar ofegante', 'Pedalar até ficar ofegante', 'Treinar musculação intensa', 'Treinar sprints em esportes'];
             var novosExercicios = '';
 
             exercicios.forEach(function(valor){
